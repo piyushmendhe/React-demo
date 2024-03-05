b1e32d9590162aa562685158080fa5901fe79288
piyushmendhe <mendhe.piyush31@gmail.com>
2024-03-05 08:38:45 +0530

    Adding BMI

--- src/Components/ListItems.js
+++ src/Components/ListItems.js
@@ -7,6 +7,7 @@ export const ListItems = ({block,OnDelete,OnHover}) => {
         <h3>Name : {block.name}</h3>
         <h5>Height : {block.height} KG</h5>
         <h5>Weight : {block.weight} CM</h5>
+        <h4 className='text-success'>BMI:{((block.weight/(block.height*block.height))*10000).toFixed(2)}</h4>
         <h5>You Tried to Delete me : {block.Hover} Times</h5>
         <button className='btn btn-warning' onMouseOver={()=>{OnHover(block)}} onClick={()=>{OnDelete(block)}}>Delete</button>
     </div>
